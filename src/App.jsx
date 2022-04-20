05ce9aa260c1eb1ffbcc3af31c5d53a98551c0d1
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-20 10:22:48 +0200

    icon.png changed and conditional rendering added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import data from "./data/data.json";
 function App() {
   const [count, setCount] = useState(0);
 
+  const title = "Info Seite";
+  const colors = ["yellow", "green", "orange", "blue"];
+  // const colors = [];
   return (
     <div className="App">
       <h1>Company Noname</h1>
@@ -18,6 +21,19 @@ function App() {
         Second employee: {data.employees[1].firstName}{" "}
         {data.employees[1].lastName}
       </div>
+
+      <h1>{title}</h1>
+      {colors.length > 0 && (
+        <>
+          <div>Color #2 : {colors[1]}</div>
+          <h3>Colors : </h3>
+          <ul>
+            {colors.map((color, index) => {
+              return <li key={index}>{color}</li>;
+            })}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
