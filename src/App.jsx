dd586d67f711133dcc91170480bc8392d0f1bff4
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-21 10:14:26 +0200

    data.json changed and all data in json datei

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,23 @@ import data from "./data/data.json";
 function App() {
   const [count, setCount] = useState(0);
 
-  const title = "Info Seite";
+  const title = "Color Palett";
   const colors = ["yellow", "green", "orange", "blue"];
   // const colors = [];
   return (
     <div className="App">
-      <h1>Company Noname</h1>
+      <h1>S Liebe Gmbh</h1>
       <img src="../public/images/icon.png" alt="logo" />
-      <div>
-        First employee: {data.employees[0].firstName}{" "}
-        {data.employees[0].lastName}
-      </div>
-      <div>
-        Second employee: {data.employees[1].firstName}{" "}
-        {data.employees[1].lastName}
-      </div>
-
+      <h2>Our Employee List</h2>
+      <ul>
+        {data.map((emp, i) => {
+          return (
+            <li>
+              {emp.firstName} {emp.lastName}
+            </li>
+          );
+        })}
+      </ul>
       <h1>{title}</h1>
       {colors.length > 0 && (
         <>
