96f878f441026a9a121125d87b6bcd0e85e2e09e
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-25 16:11:48 +0200

    ex- and internal User changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Employees } from "./components/Employees";
 
-const status = `internalUser`;
+const status = `externalUser`;
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,7 +13,6 @@ function App() {
   return (
     <div className="App">
       <Header status={status} />
-
       <Employees status={status} />
       <Footer status={status} />
     </div>
